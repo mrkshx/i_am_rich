b0329d2c9a88347650c1d3f87e6351479dcdd7f0
Markus Mengel <markus.mengelhx@gmail.com>
2020-04-02 19:18:13 +0200

    centered the image

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,17 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('I am Rich'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        backgroundColor: Colors.blueGrey,
-        body: Image(
-          image: NetworkImage(
-              'https://cdn.mos.cms.futurecdn.net/iuWB2NM48R2r9q7QhyJfhe-320-80.jpg'),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('I am Rich'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          backgroundColor: Colors.blueGrey,
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://cdn.mos.cms.futurecdn.net/iuWB2NM48R2r9q7QhyJfhe-320-80.jpg'),
+            ),
+          )),
     ),
   );
 }
